0920b67074013f89b4a1ee1b99005b89ee333ff0
james-tru <james@trufintech.io>
2024-12-23 12:54:36 +0000

    Treewalker, everforest and markview setup

---
+++ lua/plugins/treewalker.lua
@@ -0,0 +1,14 @@
+return {
+  "aaronik/treewalker.nvim",
+  opts = {
+    lazy = false,
+    highlight = true, -- Whether to briefly highlight the node after jumping to it
+    highlight_duration = 250, -- How long should above highlight last (in ms)
+  },
+  keys = {
+    { "<C-j>", "<cmd>Treewalker Down<CR>", desc = "Treewalker Down" },
+    { "<C-k>", "<cmd>Treewalker Up<CR>", desc = "Treewalker Up" },
+    { "<C-h>", "<cmd>Treewalker Left<CR>", desc = "Treewalker Parent" },
+    { "<C-l>", "<cmd>Treewalker Right<CR>", desc = "Treewalker Child" },
+  },
+}
